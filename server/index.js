37bf91bb5d88d9ee7c05b668946b7b4528176c43
Codewithgoostyhuman <zeeshantahiralvi123@gmail.com>
2026-03-03 13:59:16 +0500

    production configurations

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ import uploadRoutes from './routes/uploadRoutes.js';
 import { handleStripeWebhook } from './controllers/orderController.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONDEND_URL,
+  credentials: true,
+}));
 
 // Stripe Webhook MUST use express.raw BEFORE express.json() is applied
 app.post('/api/orders/webhook', express.raw({ type: 'application/json' }), handleStripeWebhook);
